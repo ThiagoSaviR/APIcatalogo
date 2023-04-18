ec0992d313a1a37faa0414a2c06c2dfc5bf33e34
ThiagoSaviR <thiagosavir@gmail.com>
2023-04-18 11:33:05 -0300

    Cria migrations populando tabelas da Database

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.VisualBasic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
 
+[Table("Categorias")]
 public class Categoria
 {
 
@@ -11,8 +14,14 @@ public class Categoria
         Produtos = new Collection<Produto>();
     }
 
+    [Key]
     public int CategoriaId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
     public ICollection<Produto>? Produtos { get; set; }
 }
